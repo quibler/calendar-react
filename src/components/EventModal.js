5cf9d7d9d5750422f532e1ba1aa1ec817a3f3449
quibler <ashwinidroid@gmail.com>
2021-12-13 04:42:04 +0530

    made event modal responsive

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -34,7 +34,7 @@ export default function EventModal() {
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form
         onSubmit={handleSubmit}
-        className="bg-white rounded-lg shadow-2xl w-1/4"
+        className="bg-white rounded-lg shadow-2xl md:w-1/4"
       >
         <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
           <span className="material-icons-outlined text-gray-400">
