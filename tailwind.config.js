014cdffd5d5f63c64f00e4ea0a4fc9a5f9e3fdd9
quibler <ashwinidroid@gmail.com>
2021-12-13 02:08:57 +0530

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans"]
+      },
+      gridTemplateColumns: {
+        "1/5" : "1fr 5fr"
+      }
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
